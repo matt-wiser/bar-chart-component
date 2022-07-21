85500eaa90f8113e21fc4b243d70e492015ee170
Matthew Wiser <matt.wiser@icloud.com>
2022-07-21 16:09:00 -0500

    initial markup, work on balance container

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-let data;
-
-async function fetchData() {
-const res = await fetch("./data.json");
-data = await res.json();
-}
-
-fetchData();
+const data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ];
+  
+  console.log(data);
